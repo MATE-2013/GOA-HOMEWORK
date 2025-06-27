7978c80764fcbd3674fd113733755a22e677b7b3
MATE-2013 <xatulejava0@gmail.com>
2025-06-27 20:03:10 +0400

    first commit

---
+++ day0/homework/lesson0.py
@@ -0,0 +1,83 @@
+print("mate tsetskhladze")
+from turtle import *
+
+#we want to paint a home
+
+#stap 1: drow a square
+speed(30)
+width(7)
+color("green")
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+
+forward(200)
+left(90)
+#and of squere
+
+
+forward(70)
+left(90)
+
+#draweng a door
+
+color("yellow")
+forward(70)
+right(90)
+
+forward(50)
+right(90)
+
+forward(70)
+
+#height of the door
+
+
+color("green")
+left(90)
+
+forward(80)
+left(90)
+
+forward(200)
+color("red")
+left(45)
+forward(150)
+left(93)
+forward(142)
+color("green")
+left(43)
+forward(100)
+left(90)
+color("Brown")
+forward(60)
+left(90)
+forward(60)
+left(90)
+forward(60)
+left(90)
+color("green")
+forward(160)
+left(90)
+forward(200)
+left(90)
+forward(100)
+left(90)
+color("Brown")
+forward(60)
+right(90)
+forward(60)
+right(90)
+forward(60)
+right(90)
+
+
+
+
+
+exitonclick()
