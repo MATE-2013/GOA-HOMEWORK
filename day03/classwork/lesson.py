4db15323f454d443e6c4a7be94d12ac81e514eed
MATE-2013 <xatulejava0@gmail.com>
2025-07-29 19:28:32 +0400

    commit

---
+++ day03/classwork/lesson.py
@@ -0,0 +1,8 @@
+print(type(20))#int
+print(type(12))#int
+print(type('hello'))#str
+print(type('111'))#str
+print(type('1.5'))#str
+print(type(2.2))#fio
+print('Group 74')#str
+print('Hello world')#str
