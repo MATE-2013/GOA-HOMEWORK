65f0da9d72f7142a2f9fa73baaddf82f4f54c644
MATE-2013 <xatulejava0@gmail.com>
2025-08-11 16:45:13 +0400

    commit

---
+++ day09/classwork/leson09.py
@@ -0,0 +1,4 @@
+for m in range(5):
+    print("hello")
+print("good bye")
+#ეს არის  control flow იმისგამოა რომ 5 დაწერ hello და 1 good bye
---
+++ day09/homework/index.py
@@ -0,0 +1,14 @@
+for i in range(4):
+    print(i)  
+
+for x in range(5):
+    print("სწავლა")
+
+for m in range(9):
+    print(m)
+
+for v in range(7):
+    print("hello")
+
+for k in range(3):
+    print("loop")
